8701d5565d16ff3dfd14b5a9de0393bb9f4e316e
Janzen Faidiban <45115034+janzenfaidiban@users.noreply.github.com>
2021-12-28 03:32:27 +0900

    Update vue.config.js

--- project-02/vue.config.js
+++ project-02/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '^/api': {
-                target: 'https://61ca05426f54ff00682aa809--sharp-booth-35a680.netlify.app:5000',
+                target: 'https://61ca05426f54ff00682aa809--sharp-booth-35a680.netlify.app',
                 // target: 'http://localhost:5000',
                 changeOrigin: true,
                 logLevel: 'debug',
